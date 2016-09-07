3f10cdb7cac8ca8611dc467a637f1f3b9be1a398
Marc Bramaud <marc.bramaud@goeuro.com>
2016-09-07 16:44:03 +0200

    OptionsValidator tests

--- spec/lib/model/tracker/optionsValidator/index.js
+++ spec/lib/model/tracker/optionsValidator/index.js
@@ -1,15 +1,25 @@
 import chai from 'chai'
+import { polyfill } from 'es6-object-assign'
 import validateOptions from './../../../../../lib/model/tracker/optionsValidator'
 import { DEFAULT_OPTIONS } from './../../../../../lib/model/tracker/options'
 
-const expect = chai.expect
+chai.should()
+polyfill()
 
 describe( 'OptionValidator', () => {
     it( 'should fail on empty object', () => {
-        expect( validateOptions( {} ) ).to.equal( false )
+        
+        ( validateOptions( {} ) ).should.equal( 'appId' )
     } )
 
     it( 'should fail on default options', () => {
-        expect( validateOptions( DEFAULT_OPTIONS ) ).to.equal( false )
+        ( validateOptions( DEFAULT_OPTIONS ) )
+            .should.equal( 'appId' )
+    } )
+
+    it( 'should pass on default options with appId set', () => {
+        const opts = Object.assign( {}, { appId: 'foo' }, DEFAULT_OPTIONS )
+        validateOptions( opts )
+            .should.equal( true )
     } )
 } )
