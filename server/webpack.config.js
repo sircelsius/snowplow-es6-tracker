5304960f5159df34247c4e131dd33a6bc6ec55ce
Marc Bramaud <marc.bramaud@goeuro.com>
2017-01-19 17:29:50 +0100

    #11 simple devserver

---
+++ server/webpack.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var jsEntryPath = path.resolve(__dirname, '..', 'lib', 'index.js');
+var htmlEntryPath = path.resolve(__dirname, '.', 'index.html');
+var buildPath = path.resolve(__dirname, '..', 'public', 'build');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        'whatwg-fetch',
+        'webpack-hot-middleware/client?reload=true',
+        jsEntryPath,
+        htmlEntryPath
+    ],
+    output: {
+        path: buildPath,
+        filename: 'bundle.js',
+    },
+    module: {
+        loaders:[
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel',
+                query: {
+                    // plugins: ['transform-object-assign'],
+                    presets: ['es2015']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: 'file?name=[name].[ext]',
+            },
+            {
+                test: /\.less$/,
+                loader: 'style!css!less'
+            },
+            {
+                test: /\.json$/,
+                loader: 'json'
+            }
+        ],
+    },
+    plugins: [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
